fa60545190284215ecd3ef8a0a3c583807719687
Gauri gandhi <gauri.gandhi4827@gmail.com>
2026-03-21 20:18:20 +0530

    Update frontend to use HuggingFace backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API =  "http://127.0.0.1:8000";
+const API = "https://gaurigandhi16-rag-notes-assistant.hf.space";
 
 export default function App() {
   const [file, setFile] = useState(null);
